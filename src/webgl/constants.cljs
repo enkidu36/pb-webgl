c240025db78b9d5b3ab45ce1219130e4318a72d4
Joe Savoie <enkidu36@yahoo.com>
2023-04-09 10:16:26 -0500

    added rendering primitives

--- src/webgl/constants.cljs
+++ src/webgl/constants.cljs
@@ -10,6 +10,18 @@
 (def COLOR-BUFFER-BIT	0x00004000) ;; Passed to clear to clear the current color buffer.
 
 
+;; Rendering primitives
+;; Constants passed to WebGLRenderingContext.drawElements() or WebGLRenderingContext.drawArrays() to specify what kind of primitive to render.
+
+;; Constant name	Value	Description
+(def  POINTS	0x0000) ;; Passed to drawElements or drawArrays to draw single points.
+(def LINES	0x0001) ;; Passed to drawElements or drawArrays to draw lines. Each vertex connects to the one after it.
+(def LINE-LOOP	0x0002) ;; Passed to drawElements or drawArrays to draw lines. Each set of two vertices is treated as a separate line segment.
+(def LINE-STRIP	0x0003) ;; Passed to drawElements or drawArrays to draw a connected group of line segments from the first vertex to the last.
+(def TRIANGLES	0x0004) ;; Passed to drawElements or drawArrays to draw triangles. Each set of three vertices creates a separate triangle.
+(def TRIANGLE-STRIP	0x0005) ;; Passed to drawElements or drawArrays to draw a connected group of triangles.
+(def  TRIANGLE-FAN	0x0006) ;; Passed to drawElements or drawArrays to draw a connected group of triangles. Each vertex connects to the previous and the first vertex in the fan.
+
 ;; Getting GL parameter information
 ;; Constants passed to WebGLRenderingContext.getParameter () to specify what information to return.
 (def COLOR-CLEAR-VALUE 0x0C22)
